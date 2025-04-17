05e6c9446f423c0badaef17108b624bc2ed54bed
Dwight Ledet <dwightledet@protonmail.com>
2025-04-17 09:04:55 -0230

    git

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
 
 
+
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
