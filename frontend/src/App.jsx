85bc0a16997f22d24535c61654ed48b2dff52dbe
Dwight Ledet <dwightledet@protonmail.com>
2025-04-27 08:36:50 +0200

    hooks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,86 +1,38 @@
-import React, {useState, useEffect} from 'react'
-import api from './api'
+import React from 'react'
+
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import HomeCards from './components/HomeCards';
+import WellListings from './components/WellListings';
+import ProjectListings from './components/ProjectListings';
 
 
 const App = () => {
-  const [projects, setProjects] = useState([]);
-  const fetchProjects = async () => {
-    const response = await api.get('/projects/');
-    setProjects(response.data)
-  };
-  const [wells, setWells] = useState([]);
-  const fetchWells = async () => {
-    const response = await api.get('/wells/');
-    setWells(response.data)
-  };
-  useEffect(() => {
-    fetchProjects();
-  }, []);
-  useEffect(() => {
-    fetchWells();
-  }, []);
-
-
 
 
   return (
-    <>
+    <>   
+    <Navbar />
+    <Hero />
+    <HomeCards />
+    <WellListings />
+    <ProjectListings />
+   
 
 
 
 
 
+ 
+ 
 
-
-
-
-    <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl uppercase">Themis</h2>
-        <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
-          Project management 
-        </p>
-      </div>
-    </div>
-
-
-    <header class="text-gray-600 body-font">
-  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-
-    <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center uppercase">
-      <a class="mr-5 hover:text-gray-900">Projects</a>
-      <a class="mr-5 hover:text-gray-900">Wells</a>
-      <a class="mr-5 hover:text-gray-900">Crew</a>
-      <a class="mr-5 hover:text-gray-900">PRA</a>
-    </nav>
-
-  </div>
-</header>
-
-
-    <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-wrap -m-4">
-      <div class="xl:w-1/3 md:w-1/2 p-4">
-        <div class="border border-gray-200 p-6 rounded-lg">
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Well Name Here 
-
-          {wells.map((well) => (
-              <tr key={well.id}>
-                <td>{well.name}</td>
-              </tr>
-            ))}
-
-          </h2>
-          <p class="leading-relaxed text-base">Current well operations.</p>
-          <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-Button</button>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
+  <section className="m-auto max-w-lg my-10 px-6">
+    <a
+      href="jobs.html"
+      className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+      >View All Projects</a
+    >
+  </section>
 </>
 
   )
---
+++ frontend/src/components/ProjectListings.jsx
@@ -0,0 +1,61 @@
+import React, {useState, useEffect} from 'react'
+import api from '../api';
+
+const ProjectListings = () => {
+
+    const [projects, setProjects] = useState([]);
+    const fetchProjects = async () => {
+      const response = await api.get('/projects/');
+      setProjects(response.data)
+    };
+
+    useEffect(() => {
+      fetchProjects();
+    }, []);
+
+
+  return (
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+    { projects.map((project) => (
+
+        <div key={project.id}>
+        <div class="bg-white rounded-xl shadow-md relative" >
+        <div class="p-4">
+          <div class="mb-6">
+            <div class="text-gray-600 my-2">{ project.name }</div>
+            <h3 class="text-xl font-bold">{ project.location }</h3>
+          </div>
+
+          <div class="mb-5">
+          { project.customer }
+          </div>
+
+          <h3 class="text-indigo-500 mb-2">{ project.project_manager }</h3>
+
+          <div class="border border-gray-100 mb-5"></div>
+
+          <div class="flex flex-col lg:flex-row justify-between mb-4">
+            <div class="text-orange-700 mb-3">
+              <i class="fa-solid fa-location-dot text-lg"></i>
+              Wells
+            </div>
+            <a
+              href="job.html"
+              class="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+            >
+             Revenue
+            </a>
+          </div>
+        </div>
+      </div>
+      </div>
+     
+
+    ))}
+</div>
+
+  )
+}
+
+
+export default ProjectListings
