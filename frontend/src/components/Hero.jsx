a94d946d8e39817a4218ee1d7b70dc517f7457e4
Dwight Ledet <dwightledet@protonmail.com>
2025-04-27 08:44:33 +0200

    colors

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = ({title = "BEST OPS", subtitle = "BEST PM for maximum awesomeness"}
   return (
   
     
-    <section className="bg-indigo-700 py-20 mb-4">
+    <section className="bg-blue-950 py-20 mb-4">
     <div
       className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
     >
