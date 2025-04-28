7f5fdfe3b45cfa1105d60e10376a29c41180c00f
Dwight Ledet <dwightledet@protonmail.com>
2025-04-28 07:52:21 +0200

    tables

--- frontend/src/components/ProjectListings.jsx
+++ frontend/src/components/ProjectListings.jsx
@@ -13,49 +13,71 @@ const ProjectListings = () => {
       fetchProjects();
     }, []);
 
-
   return (
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-    { projects.map((project) => (
-
-        <div key={project.id}>
-        <div class="bg-white rounded-xl shadow-md relative" >
-        <div class="p-4">
-          <div class="mb-6">
-            <div class="text-gray-600 my-2">{ project.name }</div>
-            <h3 class="text-xl font-bold">{ project.location }</h3>
-          </div>
-
-          <div class="mb-5">
-          { project.customer }
-          </div>
-
-          <h3 class="text-indigo-500 mb-2">{ project.project_manager }</h3>
-
-          <div class="border border-gray-100 mb-5"></div>
-
-          <div class="flex flex-col lg:flex-row justify-between mb-4">
-            <div class="text-orange-700 mb-3">
-              <i class="fa-solid fa-location-dot text-lg"></i>
-              Wells
-            </div>
-            <a
-              href="job.html"
-              class="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-            >
-             Revenue
-            </a>
+           <div className="px-4 sm:px-6 lg:px-8">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-base font-semibold text-gray-900">Projects</h1>
+          <p className="mt-2 text-sm text-gray-700">
+            A list of all the projects in the database.
+          </p>
+        </div>
+        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+          <button
+            type="button"
+            className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            +
+          </button>
+        </div>
+      </div>
+      <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                    Name
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Title
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Email
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Role
+                  </th>
+                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                    <span className="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {projects.map((project) => (
+                  <tr key={project.id}>
+                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                      {project.name}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{project.name}</td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{project.name}</td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{project.name}</td>
+                    <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                      <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                        Edit<span className="sr-only">, {project.name}</span>
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-      </div>
+    </div>
      
-
-    ))}
-</div>
-
   )
 }
-
 
 export default ProjectListings
